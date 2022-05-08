b9939b58a5fdd230526403aa42e76b649f26d145
Artur <artur.horch@gmx.de>
2022-05-08 17:41:56 +0200

    - implement pause button function - fix play button bug

--- src/Audio.js
+++ src/Audio.js
@@ -5,27 +5,40 @@ import {PauseFill, PlayFill} from "react-bootstrap-icons";
 export default class Audio extends React.Component{
     state = {
         song: process.env.PUBLIC_URL + 'freejazz.wav',
-        audioCtx: null,
-        buffer: null,
+        audioCtx: new AudioContext(),
+        isPlaying: false,
     }
     render() {
         return (
             <div>
                 <Button
                     variant="outline-light"
-                    onClick={() => this.load()}>
+                    onClick={() => this.play()}>
                     <PlayFill size="30" />
                 </Button>{' '}
                 <Button variant="outline-light"
-                        onClick={() => this.play()}>
+                        onClick={() => this.pause()}>
                 <PauseFill size="30" /></Button>{' '}
             </div>
         )
     }
-    load() {
-        let audioCtx = new AudioContext();
-        let source = audioCtx.createBufferSource();
 
+    play() {
+
+        if(this.state.isPlaying === true)
+        {
+            return;
+        }
+
+        if(this.state.audioCtx.state === "suspended")
+        {
+            this.state.audioCtx.resume()
+            this.state.isPlaying = true;
+            return;
+        }
+
+        let audioCtx = this.state.audioCtx;
+        let source = audioCtx.createBufferSource();
         let request = new XMLHttpRequest();
 
         request.open('GET', 'example.mp3', true);
@@ -34,7 +47,8 @@ export default class Audio extends React.Component{
         request.onload = function() {
             let audioData = request.response;
             console.log(audioCtx);
-            audioCtx.decodeAudioData(audioData, function(buffer) {
+
+            audioCtx.decodeAudioData(audioData, function (buffer) {
                 console.log("start");
                 source.buffer = buffer;
                 source.connect(audioCtx.destination);
@@ -45,6 +59,14 @@ export default class Audio extends React.Component{
             });
         };
         request.send();
+        this.state.audioCtx = audioCtx;
+        this.state.isPlaying = true;
+    }
+
+    pause()
+    {
+        this.state.isPlaying = false;
+        this.state.audioCtx.suspend();
     }
 }
 
